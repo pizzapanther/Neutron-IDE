2693bd5506ff77a3f497178627be37c08276d7d4
Paul Bailey <paul.m.bailey@gmail.com>
2011-07-02 23:00:20 -0500

    updated about

--- neutron/ide/static/ide/js/menu.js
+++ neutron/ide/static/ide/js/menu.js
@@ -1,4 +1,4 @@
 
 function about () {
-  alert('Neutron IDE v0.1 by Paul M Bailey - paul.m.bailey@gmail.com\n\nhttp://neutronide.com/\n\nLicense: GPLv3');
+  alert('Neutron IDE v0.1 by Paul M Bailey - paul.m.bailey@gmail.com\n\nneutronide.com\n\nLicense: GPLv3');
 }
