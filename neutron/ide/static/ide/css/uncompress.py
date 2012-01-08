805253f50a40307103deff7c7bc5b700d915c756
Paul Bailey <paul.m.bailey@gmail.com>
2012-01-08 09:51:37 -0600

    #28

---
+++ neutron/ide/static/ide/css/uncompress.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import shutil
+
+REPLACES = (
+  (';', ';\n  '),
+  (':', ': '),
+  ('>', ' > '),
+  ('{', ' {\n  '),
+  ('}', ';\n}\n\n'),
+  (',\.', ', .'),
+)
+
+def runme ():
+  for fp in sys.argv[1:]:
+    #shutil.copy2(fp, fp + '.old')
+    
+    fh = open(fp, 'r')
+    data = fh.read()
+    fh.close()
+    
+    for r in REPLACES:
+      data = re.sub(r[0], r[1], data)
+      
+    print data
+    #fh = open(fp, 'w')
+    #fh.write(data)
+    #fh.close()
+    
+if __name__ == '__main__':
+  runme()
+  
